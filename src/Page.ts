dd11dda3670714825a8836f778061b404059b6e7
Alexander Reid <alexreid801@gmail.com>
2019-06-11 12:07:19 -0600

    Fixed the name to be Page
    The name was Pageable, but that is the name of the input parameter.  What you get back is a Page.  Kinda like SpringDataJPA.

--- src/Page.ts
+++ src/Page.ts
@@ -1,6 +1,6 @@
-export class Pagination<PaginationObject> {
+export class Page<Entity> {
   constructor(
-    public readonly items: PaginationObject[],
+    public readonly items: Entity[],
     public readonly itemCount: number,
     public readonly totalItems: number,
     public readonly pageCount: number,
